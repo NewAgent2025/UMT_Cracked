8467e735c050f2de72d3bf50825460795440f964
NewAgent’s Demon <newagent.131+newagent2025@gmail.com>
2026-04-18 07:27:03 -0700

    Update sign-out.php
    UMT_Cracked Fixed For Overlay

--- sign-out.php
+++ sign-out.php
@@ -2,6 +2,17 @@
 // Show errors while testing (remove in production)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+$file = 'Sign_Out_Count.txt';
+
+if (!file_exists($file)) {
+    file_put_contents($file, '0');
+}
+
+$count = (int)file_get_contents($file);
+
+$count++;
+
+file_put_contents($file, $count);
 
 // Start session if it exists
 if (session_status() === PHP_SESSION_NONE) {
